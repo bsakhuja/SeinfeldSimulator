df4dec3128eba51d1ca377927d9dafabf66fa54d
bsakhuja <briansakhuja@gmail.com>
2018-01-23 22:38:58 -0800

    why isntthis working

--- seinfeldsimulator.py
+++ seinfeldsimulator.py
@@ -21,13 +21,13 @@ consumer_key_secret = os.environ.get('cons_secret')
 access_token = os.environ.get('access_token')
 access_token_secret = os.environ.get('access_token_secret')
 tweetbot.twitter_login(consumer_key, consumer_key_secret, access_token, access_token_secret)
-minutesToWait = 30
-secondsToWait = 60 * minutesToWait
-tweetbot.twitter_tweeting_start(days=0, hours=0, minutes=minutesToWait, keywords=None, prefix=None, suffix=None)
+# minutesToWait = 30
+# secondsToWait = 60 * minutesToWait
+# tweetbot.twitter_tweeting_start(days=0, hours=0, minutes=minutesToWait, keywords=None, prefix=None, suffix=None)
 
-# Start periodically tweeting
-# while True:
-# 	minutesToWait = 30
-# 	secondsToWait = 60 * minutesToWait
-# 	tweetbot.twitter_tweeting_start(days=0, hours=0, minutes=minutesToWait, keywords=None, prefix=None, suffix=None)
-# 	time.sleep(secondsToWait) # tweet every half hour
+Start periodically tweeting
+while True:
+	minutesToWait = 30
+	secondsToWait = 60 * minutesToWait
+	tweetbot.twitter_tweeting_start(days=0, hours=0, minutes=minutesToWait, keywords=None, prefix=None, suffix=None)
+	time.sleep(secondsToWait) # tweet every half hour
